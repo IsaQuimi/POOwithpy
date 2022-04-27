9eda8874194903ef60710a13171d2f5476dfabb4
Isa Quimi <quimiisabel@gmail.com>
2022-04-27 09:48:53 -0500

    commit practica para github

---
+++ abstraccion.py
@@ -0,0 +1,31 @@
+
+class Lavadora:
+    
+    def __init__(self):
+        pass
+
+    def lavar(self, temperatura= "frio"):
+        self.temperatura = temperatura
+        self._llenar_tanque_de_agua(temperatura)
+        self._añadir_jabon()
+        self._lavar()
+        self._centrifugar()
+
+    
+    def _llenar_tanque_de_agua(self, temperatura):
+        print(f"Llenando el tanque con agua {temperatura}")
+
+    def _añadir_jabon(self):
+        print("añadiendo jabon")
+
+    def _lavar(self):
+        print("lavando")
+
+    def _centrifugar(self):
+        print("centrifugando")
+
+
+
+if __name__ == "__main__":
+    lavadora = Lavadora()
+    lavadora.lavar() 
